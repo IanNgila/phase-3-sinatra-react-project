62f3a9b44b6612b591c8360f8206c6f51bfc0b2a
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-09-08 09:02:20 +0300

    Project table created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 puts "🌱 Seeding spices..."
 
+Programming.create(name: "javascript", library: React. )
+Programming.create(name: "java",library: Scala_Library)
+Programming.create(name: "python", library: Numpy)
+Programming.create(name: "Ruby", library:  Ruby_on_Rails_Gems.)
+
 # Seed your database here
+
+project.create(title: "real estate", details: " Also includes html,css,ruby.The backend is done by ruby.", reference: "https://www.tutorialrepublic.com/php-tutorial/php-mysql-crud-application.php","https://www.w3schools.com/sql/sql_create_table.asp","https://www.w3schools.com/js/js_api_intro.asp" video_links: "https://youtu.be/y47gYvXchXM","https://youtu.be/bJ05w7_DG4E" source_code: "N/A", programming_language_id: 1 )
+project.create(title: "currency converter", details: "Also has css and shell.", reference: "https://www.aalpha.net/articles/how-to-develop-a-currency-convertor-app/", video_links: "https://youtu.be/vYexzIPxObs, https://youtu.be/QIQPoaKShPg", source_code: "N/A", programming_language_id: 2)
+project.create(title: "to do list", details: ".", reference: "https://www.w3schools.com/howto/howto_js_todolist.asp,", video_links: "https://youtu.be/2QIMUBilooc", source_code: "https://www.daulathussain.com/todo-list-html-css-javascript-with-source-code/", programming_language_id: 1)
+project.create(title: "Create a code generator", details: ".", reference: "https://www.itemis.com/en/yakindu/state-machine/documentation/user-guide/codegen_python_code_generator", video_links: 18, source_code: "N/A", programming_language_id: 3)
+project.create(title: "to do list", details: ".", reference: 2016, video_links: 18, source_code: "N/A", programming_language_id: 4)
 
 puts "✅ Done seeding!"
